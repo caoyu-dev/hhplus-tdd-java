b3b6c5b51397d0cedb2c7ba5251aa19fbddb9145
caoyu <choyumin01@gmail.com>
2024-09-27 00:05:15 +0900

    feat. PointController 에서 dto 로 통신

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -1,10 +1,14 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.point.dto.HistoryResponse;
+import io.hhplus.tdd.point.dto.PointRequest;
+import io.hhplus.tdd.point.dto.PointResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
@@ -23,43 +27,45 @@ public class PointController {
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
      */
     @GetMapping("{id}")
-    public ResponseEntity<?> point(
-            @PathVariable long id
+    public ResponseEntity<PointResponse> point(
+            @PathVariable("id") long id
     ) {
-        return ResponseEntity.ok().body(pointService.getPoint(id));
+        UserPoint userPoint = pointService.getPoint(id);
+        return ResponseEntity.ok().body(PointResponse.of(userPoint));
     }
 
     /**
      * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
      */
     @GetMapping("{id}/histories")
-    public ResponseEntity<?> history(
-            @PathVariable long id
+    public ResponseEntity<List<HistoryResponse>> history(
+            @PathVariable("id") long id
     ) {
-        return ResponseEntity.ok().body(pointService.getHistory(id));
+        List<PointHistory> pointHistories = pointService.getHistory(id);
+        return ResponseEntity.ok().body(pointHistories.stream().map(HistoryResponse::of).toList());
     }
 
     /**
      * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
      */
     @PatchMapping("{id}/charge")
-    public CompletableFuture<ResponseEntity<?>> charge(
-            @PathVariable long id,
-            @RequestBody long amount
+    public CompletableFuture<ResponseEntity<PointResponse>> charge(
+            @PathVariable("id") long id,
+            @RequestBody PointRequest request
     ) {
-        return pointService.chargePoints(id, amount)
-                .thenApply(updatedUserPoint -> ResponseEntity.ok().body(updatedUserPoint));
+        return pointService.chargePoints(id, request)
+                .thenApply(updatedUserPoint -> ResponseEntity.ok().body(PointResponse.of(updatedUserPoint)));
     }
 
     /**
      * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
      */
     @PatchMapping("{id}/use")
-    public CompletableFuture<ResponseEntity<?>> use(
-            @PathVariable long id,
-            @RequestBody long amount
+    public CompletableFuture<ResponseEntity<PointResponse>> use(
+            @PathVariable("id") long id,
+            @RequestBody PointRequest request
     ) {
-        return pointService.usePoints(id, amount)
-                .thenApply(updatedUserPoint -> ResponseEntity.ok().body(updatedUserPoint));
+        return pointService.usePoints(id, request)
+                .thenApply(updatedUserPoint -> ResponseEntity.ok().body(PointResponse.of(updatedUserPoint)));
     }
 }
